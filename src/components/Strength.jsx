1e724e161323b03bddd64b9b8868c4746f2f87c4
mdsajalali <mdsajalali2020@gmail.com>
2023-10-03 17:49:06 +0600

    strength section complete

--- src/components/Strength.jsx
+++ src/components/Strength.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from "react";
+import strength1 from "../assets/images/strength-1.png";
+import strength2 from "../assets/images/strength-2.png";
+import strength3 from "../assets/images/strength-3.png";
+import strength4 from "../assets/images/strength-4.png";
 
 const Strength = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
-      <div>
-        <div></div>
+      <div className="flex gap-5 items-center justify-center">
+        <div>
+          <img className="w-40 " src={strength1} alt="" />
+        </div>
         <div>
           <p>
             More than 35 successful years of experience in the real estate
@@ -12,8 +18,41 @@ const Strength = () => {
           </p>
         </div>
       </div>
+      <div className="flex gap-5 items-center justify-center">
+        <div>
+          <img className="w-40 " src={strength2} alt="" />
+        </div>
+        <div>
+          <p>
+            Dubai partners, investors & consumers who rely on our services and
+            keeps us as the first priority
+          </p>
+        </div>
+      </div>
+      <div className="flex gap-5 items-center justify-center">
+        <div>
+          <img className="w-40 " src={strength3} alt="" />
+        </div>
+        <div>
+          <p>
+            Comprehensive understanding of the Dubai real estate market
+            Collaboration
+          </p>
+        </div>
+      </div>
+      <div className="flex gap-5 items-center justify-center">
+        <div>
+          <img className="w-40 " src={strength4} alt="" />
+        </div>
+        <div>
+          <p>
+            Collaboration with industry experts, which lets us have a good
+            knowledge of the industry updates and requirements
+          </p>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default Strength
+export default Strength;
--- src/pages/OurStrength.jsx
+++ src/pages/OurStrength.jsx
@@ -3,7 +3,7 @@ import Strength from '../components/Strength';
 
 const OurStrength = () => {
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto my-32">
       <div className="text-white text-center my-12">
         <h1 className="text-4xl">OUR STRENGTH</h1>
         <p className="border-t-4 border-indigo-500 w-16 h-2 mx-auto my-2"></p>
