2066a6f06607467c7ec542108ca8043d889a4312
mdsajalali <mdsajalali2020@gmail.com>
2024-01-18 22:41:31 +0600

    project complete

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,11 +6,11 @@ const About = () => {
     <div className="hero min-h-[70vh] bg-[#D9CEC0] px-10 md:px-0">
       <div className="hero-content flex-col gap-10 lg:flex-row-reverse">
         <div className="flex-1">
-          <h1 className="mb-5 text-3xl text-black">CORPORATE VIDEO</h1>
+          <h1 className="mb-5 text-2xl md:text-3xl text-black">CORPORATE VIDEO</h1>
           <img src={Grovy} />
         </div>
         <div className="flex-1 text-black">
-          <h1 className="text-5xl font-bold">ABOUT US</h1>
+          <h1 className="md:text-5xl text-3xl font-bold">ABOUT US</h1>
           <div className=" mt-5 w-14 border-t-4 border-[#DD9933] "></div>
           <p className="py-6">
             <p>Grovy - Real Estate Developers in Dubai</p>
--- src/pages/Tour.jsx
+++ src/pages/Tour.jsx
@@ -6,8 +6,8 @@ const Tour = () => {
     <div className="bg-[#4A3833] px-10 py-10 md:px-0">
       <div className="container mx-auto">
         <div className="my-16 text-center text-white">
-          <p className="  mx-auto my-5 h-1 w-16 bg-red-600 "></p>
           <h1 className="text-4xl  ">TAKE A TOUR</h1>
+          <div className="mx-auto my-5 h-1 w-16 bg-[#DD9933]"></div>
           <p className="my-5 text-[18px]  ">
             At GROVY, we believe in ‘Crafting liveable art in Dubai’ with our
             awe-inspiring projects.
